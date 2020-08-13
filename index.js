be0c7200bea67e2d437a7fea8d2452d343595889
zhaocl1997 <674351624@qq.com>
2020-08-13 14:23:37 +0800

    test

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-exports = module.exports = (function () {
-    'use strict'
-
-    return {
-        storage: require('./lib/storage'),
-        time: require('./lib/time'),
-        utils: require('./lib/utils'),
-        throttle: require('./lib/throttle'),
-        debounce: require('./lib/debounce'),
-        type: require('./lib/typeof'),
-        regex: require('./lib/regex')
-    }
-})()
+exports = module.exports = module.exports.default = {
+    arr: require('./lib/Array'),
+    debounce: require('./lib/Debounce'),
+    math: require('./lib/Math'),
+    obj: require('./lib/Object'),
+    regex: require('./lib/Regex'),
+    storage: require('./lib/Storage'),
+    throttle: require('./lib/Throttle'),
+    time: require('./lib/Time'),
+    type: require('./lib/Type'),
+    url: require('./lib/Url'),
+    utils: require('./lib/utils'),
+}
